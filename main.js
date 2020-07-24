617da96138414ba2d10ca0bd1f436304676f2cd9
Enrico <enricosellanora96@gmail.com>
2020-07-24 11:27:28 +0200

    modifica compl

--- main.js
+++ main.js
@@ -15,112 +15,89 @@ Consigli e domande del giorno:
 Abbiamo visto assieme una libereria che serve per gestire le date... quale sarà?
 Una chiamata ajax può anche non andare a buon fine, che si fa in quel caso? Lasciamo l'utente ad attendere? ;) */
 
-function next(mese){
-  var currentMonth = moment('2018-'+ (mese+1) , 'YYYY-M');
+function stampaMese(month, currentMonth){
   var nDayForMount = currentMonth.daysInMonth();
-  var template = $('#template').html();
+  var template = $("#template").html();
   var compiled = Handlebars.compile(template);
-  var target = $('.container ul');
-  target.html('');
+  var target = $('.container div#' + month);
+
   for (var i = 1; i <= nDayForMount; i++) {
-    var datecomplete = moment({ 'year' : 2018 , 'month' : mese , 'day' : i});
-    var dayHtml = compiled({
-      'day': i,
-      'datecomplete': datecomplete.format('YYYY-MM-DD')
+    var datecomplete = moment( { 'year' :currentMonth.year(), 'month' :currentMonth.month(), 'day' :i} );
+    var dayHTML = compiled({
+      "day": i,
+      "datecomplete": datecomplete.format("YYYY-MM-DD")
     });
-    target.append(dayHtml);
+    target.append(dayHTML);
   }
-  $.ajax({
-        url: 'https://flynn.boolean.careers/exercises/api/holidays',
-        method: 'GET',
-        data:{
-          'year':2018,
-          'month': mese
-        },
-        success: function(data) {
-          var success = data['success'];
-          var arr = data['response'];
-
-            for (var i = 0; i < arr.length; i++) {
-              var datearray = arr[i].date ;
-              $('li[data-datecomplete="'+ datearray +'"]').addClass('festa');
-              $('li[data-datecomplete="'+ datearray +'"]').append('  ('+arr[i].name+')');
-            }
-        },
-        error: function(err) {
-            console.log('err', err);
-        }
-    });
 }
-function prev(mese){
-  var currentMonth = moment('2018-'+ (mese+1) , 'YYYY-M');
-  var nDayForMount = currentMonth.daysInMonth();
-  var template = $('#template').html();
-  var compiled = Handlebars.compile(template);
-  var target = $('.container ul');
-  target.html('');
-  for (var i = 1; i <= nDayForMount; i++) {
-    var datecomplete = moment({ 'year' : 2018 , 'month' : mese , 'day' : i});
-    var dayHtml = compiled({
-      'day': i,
-      'datecomplete': datecomplete.format('YYYY-MM-DD')
-    });
-    target.append(dayHtml);
-  }
-  $.ajax({
-        url: 'https://flynn.boolean.careers/exercises/api/holidays',
-        method: 'GET',
-        data:{
-          'year':2018,
-          'month': mese
-        },
-        success: function(data) {
-          var success = data['success'];
-          var arr = data['response'];
-
-            for (var i = 0; i < arr.length; i++) {
-              var datearray = arr[i].date ;
-              $('li[data-datecomplete="'+ datearray +'"]').addClass('festa');
-              $('li[data-datecomplete="'+ datearray +'"]').append('  ('+arr[i].name+')');
-            }
-        },
-        error: function(err) {
-            console.log('err', err);
-        }
-    });
+function stampaFeste(month, currentMonth){
+  var year = currentMonth.year();
+  var month = currentMonth.month();
+  $.ajax({url: "https://flynn.boolean.careers/exercises/api/holidays",
+    method: "GET",
+    data: {
+      "year": year,
+      "month": month
+    },
+    success: function (data) {
+    var arr = data["response"];
+    for (var i = 0; i < arr.length; i++) {
+      var datearray = arr[i].date ;
+      $('h2[data-datecomplete="'+ datearray +'"]').addClass('festa');
+      $('h2[data-datecomplete="'+ datearray +'"]').append('  ('+arr[i].name+')');
+    }
+  },
+    error: function(err) {
+      console.log('err', err);
+    }
+  });
 }
-
-
-function stampaMese(){
-  var mese = 0;
-  $('.fa-angle-right').click(function(){
-  console.log(mese);
-  if (mese>=0 & mese<=11) {
-    next(mese);
-    mese++;
-  } else{
-    mese = 0;
-    alert('è il calendario del 2018 la nostra api non vuole far vedere gli altri anni');
-  }
-});
-$('.fa-angle-left').click(function(){
-console.log(mese);
-if (mese>=0 & mese<=11) {
-  prev(mese);
-  mese--;
-} else{
-  mese = 11;
-  alert('è il calendario del 2018 la nostra api non vuole far vedere gli altri anni');
+function next(){
+  var month = $("div.active");
+  var h2 = $("h2.active");
+  if(month.next().length != 0){
+    var nextMonth = month.next();
+    nextMonth.removeClass("none");
+    nextMonth.addClass("active");
+    month.removeClass("active");
+    month.addClass("none");
+    var nexth2 = h2.next();
+    nexth2.removeClass("none");
+    nexth2.addClass("active");
+    h2.removeClass("active");
+    h2.addClass("none");
+      }
 }
-
-});
+function prev(){
+  var month = $("div.active");
+  var h2 = $("h2.active");
+  if(month.prev().length != 0){
+    var prevMonth = month.prev();
+    prevMonth.removeClass("none");
+    prevMonth.addClass("active");
+    month.removeClass("active");
+    month.addClass("none");
+    var prevh2 = h2.prev();
+    prevh2.removeClass("none");
+    prevh2.addClass("active");
+    h2.removeClass("active");
+    h2.addClass("none");
+      }
 }
 
 
 function init() {
+ var year =2018;
+ var month = 0;
 
-stampaMese();
-
+ for (var i = 1; i <=12 ; i++) {
+  month = i;
+  var currentMonth = moment(year + "-" + month + "-01");
+  stampaMese(month, currentMonth);
+  stampaFeste(month, currentMonth);
+ }
+ $('.fa-angle-right').click(next);
+ $('.fa-angle-left').click(prev);
 }
 
 $(document).ready(init);
